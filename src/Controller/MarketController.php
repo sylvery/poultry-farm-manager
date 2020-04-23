39d37ba0450a930110f09d17500016414a931323
sylvery <silvery4543@gmail.com>
2020-04-24 07:28:12 +1000

    Added MarketController

--- src/Controller/MarketController.php
+++ src/Controller/MarketController.php
@@ -2,19 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Batch;
 use App\Repository\BatchRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/market")
+ */
 class MarketController extends AbstractController
 {
     /**
-     * @Route("/market", name="market_index")
+     * @Route("/", name="market_index")
      */
     public function index(BatchRepository $br)
     {
         return $this->render('market/index.html.twig', [
             'batches' => $br->findAllOnSale()
+        ]);
+    }
+
+    /**
+     * @Route("/batch/{id}", name="market_batch")
+     */
+    public function batch(Batch $batch): Response
+    {
+        return $this->render('market/batch.html.twig', [
+            'batch' => $batch
         ]);
     }
 }
